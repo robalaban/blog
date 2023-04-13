e42c3ab53a26d852ebe1d11ce08af3c404b0a745
Robert Balaban <rob.balaban@gmail.com>
2023-04-13 22:08:47 +0300

    feat: port typography settings into theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,20 @@ module.exports = {
   },
   darkMode: 'class',
   theme: {
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+    },
+    lineHeight: {
+      normal: "1.75",
+      relaxed: "1.77",
+      loose: "1.8",
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
