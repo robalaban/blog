65b510664ac72183e947ef066d235cb5ed8019f1
Robert Balaban <rob.balaban@gmail.com>
2023-04-16 23:46:11 +0300

    chore: remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: 'server',
+  adapter: cloudflare({ mode: "directory" }),
   integrations: [
     react(),
     tailwind(),
